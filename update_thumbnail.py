7076b859f6919962aa4c256ecdf17a64d0178baa
JiashengWu <jiashenw@usc.edu>
2020-03-10 23:02:18 -0700

    Add thumbnails updating script

---
+++ update_thumbnail.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+from pdf2image import convert_from_path
+from PIL import Image
+
+resume_name = 'Jiasheng_Wu_Resume_SDE'
+sizes = [
+	(1200, 627) # LinkedIn
+]
+
+curr_dir = os.path.split(os.path.realpath(__file__))[0]
+pdf_path = os.path.join(curr_dir, '{}.pdf'.format(resume_name))
+
+for size in sizes:
+	png_path = '{}_{}x{}.png'.format(resume_name, size[0], size[1])
+	png = convert_from_path(pdf_path, fmt='png', single_file=True, size=(size[0], None))[0]
+	_, height = png.size
+	png = png.crop((0, 0, size[0], size[1]))
+	png.save(os.path.join(curr_dir, png_path))
